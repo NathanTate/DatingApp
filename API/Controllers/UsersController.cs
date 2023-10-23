875b225f1356daca4ac963a9895c5c0c29b2c4ac
NathanTate <NathanKhomych@gmail.com>
2023-10-23 14:17:33 +0300

    End of section 6

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -25,6 +25,21 @@ public class UsersController : BaseApiController
         return users;
     }
 
+    [AllowAnonymous]
+    [HttpGet("search")]
+
+    public async Task<IEnumerable<AppUser>> SearchUser(string userName)
+    {
+        IEnumerable<AppUser> users = await _context.Users.ToListAsync();
+
+        if (!string.IsNullOrEmpty(userName))
+        {
+            users = users.Where(u => u.UserName.Contains(userName));
+        }
+
+        return users;
+    }
+
 
     [HttpGet("{id}")] // /api/users/2
 
