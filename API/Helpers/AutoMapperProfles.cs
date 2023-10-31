2265e2f7406588384fd001b46bb018dfbca57455
NathanTate <NathanKhomych@gmail.com>
2023-10-31 23:12:28 +0200

    End of section 12

--- API/Helpers/AutoMapperProfles.cs
+++ API/Helpers/AutoMapperProfles.cs
@@ -15,5 +15,6 @@ public class AutoMapperProfles : Profile
         .ForMember(dest => dest.Age, opt => opt.MapFrom(src => src.DateOfBirth.CalculateAge()));
         CreateMap<Photo, PhotoDto>();
         CreateMap<MemberUpdateDto, AppUser>();
+        CreateMap<RegisterDto, AppUser>();
     }
 }
--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,6 +1,9 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { AccountService } from '../_services/account.service';
 import { ToastrService } from 'ngx-toastr';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { getMinutes } from 'ngx-bootstrap/chronos/utils/date-getters';
 
 @Component({
   selector: 'app-register',
@@ -9,22 +12,59 @@ import { ToastrService } from 'ngx-toastr';
 })
 export class RegisterComponent implements OnInit{
   @Output() cancelRegister = new EventEmitter();
- model: any = {}
+ visible: boolean = true;
+ changeType: boolean = true;
+ registerForm: FormGroup = new FormGroup({});
+ maxDate: Date = new Date();
+ validationErrors: string[] | undefined;
 
-  constructor(private accountService: AccountService, private toastr: ToastrService) {}
+  constructor(private accountService: AccountService, 
+    private fb: FormBuilder, private toastr: ToastrService , private router: Router) {}
 
   ngOnInit(): void {
+    this.initializeForm();
+    this.maxDate.setFullYear(this.maxDate.getFullYear() - 18);
+  }
+
+  initializeForm() {
+    this.registerForm = this.fb.group({
+      gender: ['male'],
+      username: ['', Validators.required],
+      knownAs: ['', Validators.required],
+      dateOfBirth: ['', Validators.required],
+      city: ['', Validators.required],
+      country: ['', Validators.required],
+      password: ['', [Validators.required, 
+        Validators.minLength(6), Validators.maxLength(16)]]
+    })
   }
 
   register() {
-    this.accountService.register(this.model).subscribe({
+    const dob = this.getDateOnly(this.registerForm.controls['dateOfBirth'].value);
+    const values = {...this.registerForm.value, dateOfBirth: dob};
+    this.accountService.register(values).subscribe({
       next: () => {
-        this.cancel();
+        this.router.navigateByUrl('/members')
+      },
+      error: error => {
+        this.validationErrors = error
       }
     })
   }
 
   cancel() {
     this.cancelRegister.emit(false);
+  }
+
+  viewpassword() {
+    this.visible = !this.visible;
+    this.changeType = !this.changeType;
+  }
+
+  private getDateOnly(dob: string | undefined) {
+    if(!dob) return;
+    let theDob = new Date(dob);
+    return new Date(theDob.setMinutes(theDob.getMinutes()-theDob.getTimezoneOffset()))
+    .toISOString().slice(0,10);
   }
 }
