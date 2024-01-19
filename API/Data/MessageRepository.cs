aaed945caf1f26446bb7742c9f8b90234d058309
NathanTate <NathanKhomych@gmail.com>
2024-01-19 18:52:49 +0200

    DateTime app crash fixes

--- API/Data/MessageRepository.cs
+++ API/Data/MessageRepository.cs
@@ -101,7 +101,7 @@ public class MessageRepository : IMessageRepository
             {
                 foreach (var message in unreadMessages)
                 {
-                    message.DateRead = DateTime.Now;
+                    message.DateRead = DateTime.UtcNow;
                 }
             }
 
